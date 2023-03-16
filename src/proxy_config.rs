1dee22db6b0224f8f032ad755c4da526f8698ac0
Kobby Pentangeli <kobbypentangeli@gmail.com>
2023-03-16 20:48:57 +0000

    Implement `ProxyConfig` to read config info from file

---
+++ src/proxy_config.rs
@@ -0,0 +1,40 @@
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use std::io::Read;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ProxyConfig {
+    block_headers: Option<Vec<String>>,
+    block_params: Option<Vec<String>>,
+    mask_params: Vec<String>,
+}
+
+impl ProxyConfig {
+    pub fn load_from_file(file_path: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let mut file = match std::fs::File::open(file_path) {
+            Ok(file) => file,
+            Err(err) => return Err(format!("Failed to open config file: {}", err).into()),
+        };
+
+        let mut buf = String::new();
+        if let Err(err) = file.read_to_string(&mut buf) {
+            return Err(format!("Failed to read config file: {}", err).into());
+        }
+
+        match serde_json::from_str(&buf) {
+            Ok(config) => Ok(config),
+            Err(err) => Err(format!("Failed to deserialize config file: {}", err).into()),
+        }
+    }
+
+    pub fn mask_response_body(&self, body: &str) -> String {
+        let mut masked_body = body.to_owned();
+        for param in &self.mask_params {
+            let re = Regex::new(&format!("{}=([^&]+)", param)).unwrap();
+            masked_body = re
+                .replace_all(&masked_body, &format!("{}=****", param))
+                .to_string();
+        }
+        masked_body
+    }
+}
